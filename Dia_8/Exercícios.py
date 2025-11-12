bcab67558138b8c1c078e5a09696c6e9f5b70f98
Wagnerjuniorr <wagnerjuniorr669@gmail.com>
2025-11-12 11:44:24 -0300

    Dia 8 exercícios

--- Dia_8/Exercícios.py
+++ Dia_8/Exercícios.py
@@ -14,7 +14,7 @@ student = {
     'gender':'male',
     'age':69,
     'maritial_status':'single',
-    'skills':['Englis','karate','Python'],
+    'skills':['English','karate','Python'],
     'Country':'BRAZIL',
     'Address':{'labububuo':'lalal',
                'lalala':'bibibi'}
@@ -22,3 +22,23 @@ student = {
 # Get the length of the student dictionary
 print(len(student))
 # Get the value of skills and check the data type, it should be a list
+print(student.get('skills'))
+print(type(student.get('skills')))
+# Modify the skills values by adding one or two skills
+student['skills'].extend(['Paito','Futebol'])
+# Get the dictionary keys as a list
+keys = student.keys()
+print(keys)
+# Get the dictionary values as a list
+values = student.values()
+print(values)
+# Change the dictionary to a list of tuples using items() method
+student = student.items()
+print(student)
+
+# Delete one of the items in the dictionary
+student = dict(student)
+del student['skills']
+print(student.get('skills'))
+# Delete one of the dictionaries
+del student
