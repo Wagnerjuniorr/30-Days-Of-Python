96df4cfccb26b3bd05bc73a6fb2db30963f10477
Wagnerjuniorr <wagnerjuniorr669@gmail.com>
2026-01-05 22:31:33 -0300

    Dia 25

---
+++ Dia_25/Pandas.py
@@ -0,0 +1,10 @@
+# Pandas is an open source, high performance, easy to use data strcutures and data analysis tools fot the python programming language
+# Pandas add data structures and tools designed to weok with table like data wich is series ana data frames
+# panda provides tools for data manipulation
+# reshaping
+# merging
+# sorting
+# slicing
+# aggregation
+# imputation
+# if you are using anaconda, you do not have to install
